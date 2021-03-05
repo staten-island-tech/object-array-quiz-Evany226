b19d1fae34d4e0550dc7af24da7a5c53c0756085
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-03-04 19:39:06 -0500

    completed
    didn't know how to do bonus question

--- app.js
+++ app.js
@@ -10,16 +10,43 @@ const presidents = [
   { name: "Richard Nixon", terms: 2, party: "R" },
   { name: "John F. Kennedy", terms: 1, party: "D" },
   { name: "Lyndon Johnson", terms: 2, party: "D" },
-  { name: "Dwight Eisenhower", terms: 2, party: "R" }
+  { name: "Dwight Eisenhower", terms: 2, party: "R" },
 ];
 
 //Challenge Questions
 //1) Filter all presidents, leaving only the Democratic ones
+const democrats = presidents.filter((presidents) => presidents.party === "D");
+console.log(democrats);
 
 //2)Filter all presidents to leave only one term Republican presidents HINT use If statement
+const republicans = presidents.filter(
+  (presidents) => presidents.party === "R" && presidents.terms === 1
+);
+console.log(republicans);
 
 //3) return only the last three presidents
+/*const lastThree = presidents.slice(9, 12);
+console.log(lastThree);
+*/
+
+const lastThree = presidents.slice(presidents.length - 3);
+console.log(lastThree);
 
 //4) log all dems who served 2 terms. HINT use chain filter, filter and slice
+const twoTermDemocrats = presidents.filter(
+  (presidents) => presidents.party === "D" && presidents.terms === 2
+);
+
+console.log(twoTermDemocrats);
 
 //BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done"
+
+const LBJ = presidents(function (term) {
+  if (term === 2) {
+    console.log("LBJ served two terms");
+  } else {
+    console.log("LBJ was one and done");
+  }
+});
+
+//not really sure how to write this one. I think I did the if/else statements correctly but i'm not sure how to specifically check only lbj
